bc766a82566938444bfa97ddbd6e5d42237a75d4
davekpython <davekantar@gmail.com>
2013-12-19 15:37:36 -0500

    imageupload

--- static/script.js
+++ static/script.js
@@ -94,6 +94,19 @@ var flag = true;
     flag=!flag;
 });
 
+function previewImage(input) {
+	var preview = document.getElementById('preview');
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			preview.setAttribute('src', e.target.result);
+		}
+		reader.readAsDataURL(input.files[0]);
+	} else {
+		preview.setAttribute('src', 'placeholder.png');
+	}
+}
+
 
 });
 
