436f6e087435d01e6636e3bc3d48b382653ba70a
davekpython <davekantar@gmail.com>
2013-12-31 12:04:17 -0500

    new

--- templates/script.js
+++
@@ -1,6 +0,0 @@
-$(permalink.html).ready(function() {
-	$('p .edit').click(function() {
-	$('#david').hide();
-		// $('p .edit').append('<input type="text" name="updatesubject" value="{{updatesubject}}" placeholder="Update Subject">');
-	});
-});
